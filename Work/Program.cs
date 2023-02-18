bb28d8f666f347b13b0a793c2647c61fb6c50074
Ritorta <meddesu@yandex.ru>
2023-02-18 14:38:04 +0300

    create array, create array check 3 characters, create show array

--- Work/Program.cs
+++ Work/Program.cs
@@ -3,7 +3,8 @@
 // Условия задачи:
 
 // Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.
-// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выпоолнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выпоолнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 // Примеры:
 
@@ -15,5 +16,52 @@
 // Решение:
 //
 
+    Console.Write("Enter the number of rows for array: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    
+    string[] strArray = CreateArray1();
+    string[] ThreeCharacters = CheekArray(strArray);
+    
+   
 
+string[] CreateArray1()
+{
+    string[] array1 = new string[size];
+
+        
+        for(int i = 0; i < array1.Length; i++)
+        {
+            Console.Write($"Enter text the rows {i + 1}: ");
+            array1[i] = Console.ReadLine();
+        }
+    
+
+return array1; 
+}
+
+void ShowArrayText(string[] array)
+{
+
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}, ");
+    }
+    Console.Write("] ");
+
+}
+
+string[] CheekArray(string[] array)
+{
+    
+    for(int i = 0; i < array.Length; i++)
+        {
+        if(array[i].Length <= 3)
+            {
+                Console.Write($" {array[i]}, ");   
+            }
+        }
+
+return array;    
+}
 
